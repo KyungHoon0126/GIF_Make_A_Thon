23ef4737d32c252f7673be4280e410a970bbfd7d
김영재 <dudwo0914@gmail.com>
2019-11-01 13:46:39 +0900

    ASG

--- app/src/main/java/com/players/gif/welcomePackage/Popup_welcome_OK.java
+++ app/src/main/java/com/players/gif/welcomePackage/Popup_welcome_OK.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.Window;
+import android.view.animation.AnimationUtils;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.players.gif.R;
@@ -13,10 +14,10 @@ public class Popup_welcome_OK extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.welcome_popup);
         getWindow().setBackgroundDrawableResource(R.color.transparent);
+        findViewById(R.id.popup).startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.size_up));
         new Thread(()->{try{
             Thread.sleep(1500);
             Intent i = new Intent();
